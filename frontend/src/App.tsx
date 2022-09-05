72ecafd9e3c104c16993a8ba4940ee35e6bd1151
p4ckysm4cky <58573901+p4ckysm4cky@users.noreply.github.com>
2022-09-05 16:59:58 +1200

    Added very basic navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,12 @@
-import React from "react";
-import logo from "./logo.svg";
+import React from 'react';
+import Navbar from './components/Navbar';
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <Navbar />
+        </div>
+    );
 }
 
 export default App;
---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import styles from './Navbar.module.css';
+import logo from './images/logo.png';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <nav>
+            <div className={styles.container}>
+                <Link to="/">
+                    <img className={styles.logo} src={logo} />
+                </Link>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
